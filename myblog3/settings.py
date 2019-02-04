a044c73dec06825eeaee61a111ba2dd3d031820a
Draftbk <draftbks@gmail.com>
2019-02-04 14:57:09 -0500

    update

--- myblog3/settings.py
+++ myblog3/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'myblog3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'awesome',
+        'USER': 'Draftbk',
+        'PASSWORD': '8288178Slf',
+        'HOST': 'awsblog.cuqtmsbllwin.us-east-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
